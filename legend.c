13ec4e2b119ba6aa316e078a6f491c8e8ab06a8d
Baptiste Pajot <bpajot@z3r10p3.le-101.fr>
2018-01-02 15:37:08 +0100

    update legend with rot

--- legend.c
+++ legend.c
@@ -6,7 +6,7 @@
 /*   By: bpajot <marvin@le-101.fr>                  +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2017/12/15 12:18:04 by bpajot       #+#   ##    ##    #+#       */
-/*   Updated: 2018/01/02 13:48:21 by bpajot      ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/01/02 15:33:25 by bpajot      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -23,12 +23,12 @@ static void		display_seprarator(t_all *all, int color)
 	all->line.color2 = all->line.color1;
 	ft_line(all, 1);
 	all->line.x1 = 0;
-	all->line.y1 = all->e.height / 3;
+	all->line.y1 = 7 * all->e.height / 30;
 	all->line.x2 = all->e.sep_width - 1;
 	all->line.y2 = all->line.y1;
 	ft_line(all, 1);
 	all->line.x1 = 0;
-	all->line.y1 = 2 * all->e.height / 3;
+	all->line.y1 = 16 * all->e.height / 30;
 	all->line.x2 = all->e.sep_width - 1;
 	all->line.y2 = all->line.y1;
 	ft_line(all, 1);
@@ -62,55 +62,57 @@ static void		display_file(t_env e, t_size size, int color_txt)
 
 static void		display_view(t_env e, t_size size, int color_txt)
 {
-	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 20, e.height / 3 + 20,
+	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 20, 7 * e.height / 30 + 20,
 		color_txt, "VIEW");
-	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 130, 12 * e.height / 30,
+	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 130, 9 * e.height / 30,
 		color_txt, "projection :");
-	mlx_string_put(e.mlx, e.win, e.sep_width / 2, 12 * e.height / 30,
+	mlx_string_put(e.mlx, e.win, e.sep_width / 2, 9 * e.height / 30,
 		color_txt, "iso");
-	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 130, 13 * e.height / 30,
+	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 130, 10 * e.height / 30,
 		color_txt, "scale_xy :");
-	mlx_string_put(e.mlx, e.win, e.sep_width / 2, 13 * e.height / 30,
+	mlx_string_put(e.mlx, e.win, e.sep_width / 2, 10 * e.height / 30,
 		color_txt, ft_strjoin(ft_strjoin(ft_itoa(size.scale_xy), "."),
 		ft_itoa((int)(size.scale_xy * 100) % 100)));
-	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 130, 14 * e.height / 30,
+	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 130, 11 * e.height / 30,
 		color_txt, "scale_z :");
-	mlx_string_put(e.mlx, e.win, e.sep_width / 2, 14 * e.height / 30,
+	mlx_string_put(e.mlx, e.win, e.sep_width / 2, 11 * e.height / 30,
 		color_txt, ft_strjoin(ft_strjoin(ft_itoa(size.scale_z), "."),
 		ft_itoa((int)(size.scale_z * 100) % 100)));
-	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 130, 15 * e.height / 30,
+	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 130, 12 * e.height / 30,
 		color_txt, "center_x :");
-	mlx_string_put(e.mlx, e.win, e.sep_width / 2, 15 * e.height / 30,
+	mlx_string_put(e.mlx, e.win, e.sep_width / 2, 12 * e.height / 30,
 		color_txt, ft_itoa(size.center_x));
-	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 130, 16 * e.height / 30,
+	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 130, 13 * e.height / 30,
 		color_txt, "center_y :");
-	mlx_string_put(e.mlx, e.win, e.sep_width / 2, 16 * e.height / 30,
+	mlx_string_put(e.mlx, e.win, e.sep_width / 2, 13 * e.height / 30,
 		color_txt, ft_itoa(size.center_y));
-	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 130, 17 * e.height / 30,
+	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 130, 14 * e.height / 30,
 		color_txt, "rot_z :");
-	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 40, 17 * e.height / 30,
+	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 40, 14 * e.height / 30,
 		color_txt, ft_itoa(size.rot_z));
 }
 
 static void		display_com(t_env e, int color_txt)
 {
-	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 90, 2 * e.height / 3 + 20,
+	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 90, 16 * e.height / 30 + 20,
 		color_txt, "KEYBOARD SHORTCUT");
-	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 130, 22 * e.height / 30,
+	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 130, 18 * e.height / 30,
 		color_txt, "<- : left");
-	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 130, 23 * e.height / 30,
+	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 130, 19 * e.height / 30,
 		color_txt, "-> : right");
-	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 130, 24 * e.height / 30,
+	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 130, 20 * e.height / 30,
 		color_txt, "/|\\ : up");
-	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 130, 25 * e.height / 30,
+	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 130, 21 * e.height / 30,
 		color_txt, "\\|/ : down");
-	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 130, 26 * e.height / 30,
-		color_txt, "1(!) / 2(@) : zoom    -/+");
-	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 130, 27 * e.height / 30,
-		color_txt, "3(#) / 4($) : zoom xy -/+");
+	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 130, 22 * e.height / 30,
+		color_txt, "1(!) / 2(@) : zoom     -/+");
+	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 130, 23 * e.height / 30,
+		color_txt, "3(#) / 4($) : zoom xy  -/+");
+	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 130, 24 * e.height / 30,
+		color_txt, "5(%) / 6(^) : zoom z   -/+");
+	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 130, 25 * e.height / 30,
+		color_txt, "7(&) / 8(*) : rotate z -/+");
 	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 130, 28 * e.height / 30,
-		color_txt, "5(%) / 6(^) : zoom z  -/+");
-	mlx_string_put(e.mlx, e.win, e.sep_width / 2 - 130, 29 * e.height / 30,
 		color_txt, "Esc or Q : Close Window");
 }
 
